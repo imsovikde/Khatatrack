5bfdeb6ae60004b56a9e469153a123e1b5434c9c
Souvik Dey <imsovikde@gmail.com>
2026-08-07 21:11:40 +0530

    ci: fix apk signature mismatch and add multi-abi splits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,15 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
+  splits {
+    abi {
+      isEnable = true
+      reset()
+      include("armeabi-v7a", "arm64-v8a")
+      isUniversalApk = true
+    }
+  }
+
   signingConfigs {
     create("release") {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
